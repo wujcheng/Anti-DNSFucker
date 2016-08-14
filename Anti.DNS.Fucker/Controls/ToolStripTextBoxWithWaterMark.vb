b79f16efa5e9129e29cf8be4fc7b823b40282502
zqmillet <zqmillet@qq.com>
2016-08-14 22:32:44 +0800

    Add the Class ToolStripTextBoxWithWaterMark

---
+++ Anti.DNS.Fucker/Controls/ToolStripTextBoxWithWaterMark.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class ToolStripTextBoxWithWaterMark
+    Inherits System.Windows.Forms.ToolStripTextBox
+
+    Shadows TextBox As TextBox
+
+
+    Private Const ECM_FIRST As UInteger = &H1500
+    Private Const EM_SETCUEBANNER As UInteger = ECM_FIRST + 1
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto, SetLastError:=False)>
+    Private Shared Function SendMessage(hWnd As IntPtr, Msg As UInteger, wParam As UInteger, <MarshalAs(UnmanagedType.LPWStr)> lParam As String) As IntPtr
+    End Function
+
+    Public Sub New()
+        TextBox = Me.Control
+
+        With TextBox
+            .BorderStyle = BorderStyle.FixedSingle
+            'AddHandler .TextChanged, AddressOf TextBox_TextChanged
+            'AddHandler .Leave, AddressOf TextBox_TextChanged
+        End With
+
+        SetWatermark("Domain Name")
+    End Sub
+
+    Private Sub SetWatermark(watermarkText As String)
+        SendMessage(TextBox.Handle, EM_SETCUEBANNER, 0, watermarkText)
+    End Sub
+
+
+    'Private Sub TextBox_TextChanged()
+    '    If TextBox.Text = "" Then
+    '        Dim Graphics As Graphics = TextBox.CreateGraphics
+    '        Graphics.DrawString("23333", (New ToolStripMenuItem).Font, SystemBrushes.GrayText, New Point(0, 2))
+    '    End If
+    'End Sub
+
+
+End Class
--- Anti.DNS.Fucker/WebAddressPanel.vb
+++ Anti.DNS.Fucker/WebAddressPanel.vb
@@ -1,5 +1,5 @@
 ﻿Public Class WebAddressPanel
-    Inherits System.Windows.Forms.Panel
+    Inherits System.Windows.Forms.ToolStrip
 
     Private EnableCheckBox As CheckBox
     Private DomainNameTextBox As WaterMarkTextBox
@@ -7,68 +7,25 @@
     Private GetIPv6AddressCheckBox As CheckBox
     Private DeleteButton As Button
 
-    Private ToolStrip As ToolStrip
+
+
 
     Const ControlSeparation As Integer = 4
 
     Public Sub New()
+        MyBase.New
+        With Me
+            .GripStyle = ToolStripGripStyle.Hidden
+            .RenderMode = ToolStripRenderMode.Professional
+            .Items.Add(New ToolStripTextBoxWithWaterMark)
+            .Items.Add(New ToolStripButton)
+            .Items.Add(New ToolStripControlHost(New CheckBox))
+        End With
+
+
         With Me
             .Width = 200
-            .Height = 27
-            .BorderStyle = BorderStyle.FixedSingle
         End With
 
-        EnableCheckBox = New CheckBox
-        With EnableCheckBox
-            .Text = ""
-            .Width = 20
-            .Location = New Point(0, 0)
-            .Anchor = AnchorStyles.Left Or AnchorStyles.Top
-            .Parent = Me
-        End With
-
-        DeleteButton = New Button
-        With DeleteButton
-            .Image = Image.FromFile(Application.StartupPath & "\Icon\Delete.ico")
-            .Width = Me.Height
-            .Height = .Width
-            .ImageAlign = ContentAlignment.MiddleCenter
-            .FlatStyle = FlatStyle.Flat
-            .FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255)
-            .FlatAppearance.BorderSize = 0
-            .BackgroundImageLayout = ImageLayout.Zoom
-
-            .Location = New Point(Me.Width - .Width, 0)
-            .Anchor = AnchorStyles.Left Or AnchorStyles.Top
-            .Parent = Me
-        End With
-
-        GetIPv6AddressCheckBox = New CheckBox
-        With GetIPv6AddressCheckBox
-            .Text = ""
-            .Width = 20
-            .Location = New Point(Me.Width - DeleteButton.Width - ControlSeparation - .Width, 0)
-            .Anchor = AnchorStyles.Right Or AnchorStyles.Top
-            .Parent = Me
-        End With
-
-        GetIPv4AddressCheckBox = New CheckBox
-        With GetIPv4AddressCheckBox
-            .Text = ""
-            .Width = 20
-            .Location = New Point(Me.Width - DeleteButton.Width - ControlSeparation - GetIPv6AddressCheckBox.Width - ControlSeparation - .Width, 0)
-            .Anchor = AnchorStyles.Right Or AnchorStyles.Top
-            .Parent = Me
-        End With
-
-        DomainNameTextBox = New WaterMarkTextBox
-        With DomainNameTextBox
-            .WaterMarkText = "Domain Name"
-            .Width = Me.Width - DeleteButton.Width - ControlSeparation - EnableCheckBox.Width - ControlSeparation - GetIPv4AddressCheckBox.Width - ControlSeparation - GetIPv6AddressCheckBox.Width - ControlSeparation
-            .Text = ""
-            .Location = New Point(EnableCheckBox.Width + ControlSeparation, 0)
-            .Anchor = AnchorStyles.Left Or AnchorStyles.Top Or AnchorStyles.Right
-            .Parent = Me
-        End With
     End Sub
 End Class
