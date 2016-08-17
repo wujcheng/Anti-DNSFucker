d8a85024544ab30a1165d30244d724636c2d45b0
zqmillet <zqmillet@qq.com>
2016-08-17 09:41:14 +0800

    Happy Birthday! Today We Are All Mogicians!

---
+++ Anti.DNS.Fucker/Controls/TextBoxWithWaterMark.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class TextBoxWithWaterMark
+    Inherits System.Windows.Forms.TextBox
+
+    Private Const ECM_FIRST As UInteger = &H1500
+    Private Const EM_SETCUEBANNER As UInteger = ECM_FIRST + 1
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto, SetLastError:=False)>
+    Private Shared Function SendMessage(hWnd As IntPtr, Msg As UInteger, wParam As UInteger, <MarshalAs(UnmanagedType.LPWStr)> lParam As String) As IntPtr
+    End Function
+
+    Public Property WaterMarkText As String
+        Get
+            Return Me.Tag
+        End Get
+        Set(value As String)
+            Me.Tag = value
+            SetWaterMark(value)
+        End Set
+    End Property
+
+    Private Sub SetWaterMark(waterMarkText As String)
+        SendMessage(Me.Handle, EM_SETCUEBANNER, 0, waterMarkText)
+    End Sub
+End Class
--- Anti.DNS.Fucker/FormMain.vb
+++ Anti.DNS.Fucker/FormMain.vb
@@ -1,4 +1,8 @@
-﻿Public Class FormMain
+﻿Imports System.IO
+Imports System.Runtime.Serialization.Formatters
+Imports System.Runtime.Serialization.Formatters.Binary
+
+Public Class FormMain
     Private TableLayoutPanelHead As TableLayoutPanel
     Private CheckBoxHeadSelectAll As CheckBox
     Private LabelHeadDomainName As Label
@@ -9,6 +13,7 @@
     Private TableLayoutPanelList As TableLayoutPanel
 
     Private ToolStrip As ToolStrip
+    Private ToolStripLabelTitle As ToolStripLabel
     Private ToolStripButtonAdd As ToolStripButton
     Private ToolStripButtonRemove As ToolStripButton
     Private ToolStripButtonQuit As ToolStripButton
@@ -16,11 +21,11 @@
 
     Private StatusStrip As StatusStrip
 
-
-    Private TopPadding As Padding = New Padding(0, 4, 0, 0)
+    Private TopPadding As Padding = New Padding(0, 7, 0, 0)
     Private TransparentColor As Color = Color.FromArgb(0, Color.Black)
     Private MouseDownLocation As Point
     Private FormLastLocation As Point
+    Private ItemHeight As Integer = 27
 
     Public Sub New()
         InitializeComponent()
@@ -34,31 +39,45 @@
 
         InitializeToolStrip()
         InitializeStatusStrip()
-        InitializeHeadTableLayoutPanel()
+        InitializeTableLayoutPanelHead()
+        InitializeTableLayoutPanelList()
     End Sub
 
     Private Sub InitializeToolStrip()
+        ToolStripLabelTitle = New ToolStripLabel
+        With ToolStripLabelTitle
+            .Text = "Anti-DNSFucker"
+
+            AddHandler .MouseDown, AddressOf ToolStrip_MouseDown
+            AddHandler .MouseUp, AddressOf ToolStrip_MouseUp
+            AddHandler .MouseMove, AddressOf ToolStrip_MouseMove
+        End With
+
         ToolStripButtonAdd = New ToolStripButton
         With ToolStripButtonAdd
             .Image = Icons.Add
+            .ToolTipText = "Add New Domain Name"
             AddHandler .Click, AddressOf ToolStripButton_Click
         End With
 
         ToolStripButtonRemove = New ToolStripButton
         With ToolStripButtonRemove
             .Image = Icons.Remove
+            .ToolTipText = "Remove Selected Domain Name"
             AddHandler .Click, AddressOf ToolStripButton_Click
         End With
 
         ToolStripButtonQuit = New ToolStripButton
         With ToolStripButtonQuit
             .Image = Icons.Quit
+            .ToolTipText = "Quit"
             AddHandler .Click, AddressOf ToolStripButton_Click
         End With
 
         ToolStripButtonSaveAs = New ToolStripButton
         With ToolStripButtonSaveAs
             .Image = Icons.SaveAs
+            .ToolTipText = "Save As"
             AddHandler .Click, AddressOf ToolStripButton_Click
         End With
 
@@ -68,6 +87,8 @@
             .RenderMode = ToolStripRenderMode.Professional
             .Parent = Me
             .BackColor = Color.White
+            .Items.Add(ToolStripLabelTitle)
+            .Items.Add(New ToolStripSeparator)
             .Items.Add(ToolStripButtonAdd)
             .Items.Add(ToolStripButtonRemove)
             .Items.Add(New ToolStripSeparator)
@@ -86,6 +107,51 @@
             End
         End If
 
+        If sender Is ToolStripButtonAdd Then
+            AddNewDomainName()
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub AddNewDomainName()
+        With TableLayoutPanelList
+            .RowStyles.Add(New RowStyle(SizeType.Absolute, ItemHeight))
+
+            Dim CheckBoxSelected As New CheckBox
+            With CheckBoxSelected
+                .Text = ""
+            End With
+
+            Dim CheckBoxEnable As New CheckBox
+            With CheckBoxEnable
+                .Text = ""
+            End With
+
+            Dim TextBoxDomainName As New TextBoxWithWaterMark
+            With TextBoxDomainName
+                .WaterMarkText = "Please Input Domain Name."
+                .Dock = DockStyle.Fill
+            End With
+
+            Dim CheckBoxGetIPv4Address As New CheckBox
+            With CheckBoxGetIPv4Address
+                .Text = ""
+            End With
+
+            Dim CheckBoxGetIPv6Address As New CheckBox
+            With CheckBoxGetIPv6Address
+                .Text = ""
+            End With
+
+            Dim Index As Integer = .RowCount
+            .Controls.Add(CheckBoxSelected, 0, Index)
+            .Controls.Add(CheckBoxEnable, 1, Index)
+            .Controls.Add(TextBoxDomainName, 2, Index)
+            .Controls.Add(CheckBoxGetIPv4Address, 3, Index)
+            .Controls.Add(CheckBoxGetIPv6Address, 4, Index)
+
+            .RowCount += 1
+        End With
     End Sub
 
     Private Sub ToolStrip_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs)
@@ -121,7 +187,7 @@
         End With
     End Sub
 
-    Private Sub InitializeHeadTableLayoutPanel()
+    Private Sub InitializeTableLayoutPanelHead()
         CheckBoxHeadSelectAll = New CheckBox
         With CheckBoxHeadSelectAll
             .Text = ""
@@ -157,9 +223,11 @@
 
         TableLayoutPanelHead = New TableLayoutPanel
         With TableLayoutPanelHead
-            .Width = Me.ClientSize.Width
-            .Height = 22
+            .Width = Me.ClientSize.Width + System.Windows.Forms.SystemInformation.VerticalScrollBarWidth
+            .Height = ItemHeight
+            .BackColor = Color.DarkGray
             .Location = New Point(0, ToolStrip.Height)
+
             .CellBorderStyle = TableLayoutPanelCellBorderStyle.Single
             .RowStyles.Add(New RowStyle(SizeType.Absolute, .Height))
             .ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 1))
@@ -167,6 +235,7 @@
             .ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 5))
             .ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 2))
             .ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 2))
+            .ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, System.Windows.Forms.SystemInformation.VerticalScrollBarWidth))
             .Controls.Add(CheckBoxHeadSelectAll, 0, 0)
             .Controls.Add(LabelHeadEnable, 1, 0)
             .Controls.Add(LabelHeadDomainName, 2, 0)
@@ -174,7 +243,26 @@
             .Controls.Add(LabelHeadGetIPv6Address, 4, 0)
             .Parent = Me
         End With
+    End Sub
 
+    Private Sub InitializeTableLayoutPanelList()
+        TableLayoutPanelList = New TableLayoutPanel
+        With TableLayoutPanelList
+            .Location = New Point(0, TableLayoutPanelHead.Height + ToolStrip.Height)
+            .Size = New Size(TableLayoutPanelHead.Width, Me.ClientSize.Height - TableLayoutPanelHead.Height - ToolStrip.Height - StatusStrip.Height)
+            .Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Top
+            ' .CellBorderStyle = TableLayoutPanelCellBorderStyle.Single
+            .HorizontalScroll.Maximum = 0
+            .AutoScroll = False
+            .HorizontalScroll.Visible = False
+            .HorizontalScroll.Enabled = False
+            .AutoScroll = True
 
+            For Each ColumnStyle As ColumnStyle In TableLayoutPanelHead.ColumnStyles
+                .ColumnStyles.Add(New ColumnStyle(ColumnStyle.SizeType, ColumnStyle.Width))
+            Next
+
+            .Parent = Me
+        End With
     End Sub
 End Class
