fba2658c15ed3d4ed0f09520c6e79a6bb8b42507
zqmillet <zqmillet@qq.com>
2016-08-27 09:46:37 +0800

    Add Some Comments

--- Anti-DNSFucker/Controls/CheckBoxAdvanced.vb
+++ Anti-DNSFucker/Controls/CheckBoxAdvanced.vb
@@ -1,6 +1,17 @@
-﻿Public Class CheckBoxAdvanced
+﻿''' <summary>
+''' This class inherits CheckBox, and it is an advanced CheckBox.
+''' Comparing with the CheckBox, CheckBoxAdvanced has a public property: Broken, which is a Boolean.
+''' If the property Broken is True, the properties Enabled and Checked cannot be operated.
+''' </summary>
+Public Class CheckBoxAdvanced
     Inherits System.Windows.Forms.CheckBox
 
+    ''' <summary>
+    ''' This is the newly added property: Broken. 
+    ''' The value of this proporty is save in the IsBroken which is a Boolean, too.
+    ''' If the property Broken is assigned True, the properties Enabled and Checked is assigned False.
+    ''' </summary>
+    ''' <returns></returns>
     Public Property Broken As Boolean
         Get
             Return IsBroken
@@ -13,8 +24,19 @@
             End If
         End Set
     End Property
+
+    ''' <summary>
+    ''' This property is used to save the value of the property Broken.
+    ''' </summary>
     Private IsBroken As Boolean
 
+    ''' <summary>
+    ''' This is property is used to override the property Enabled.
+    ''' If this property is read, there is nothing different.
+    ''' But if this property is assigned, the property Broken is checked first.
+    ''' If the property Broken is True, the property Enabled does not work.
+    ''' </summary>
+    ''' <returns></returns>
     Public Shadows Property Enabled As Boolean
         Get
             Return MyBase.Enabled
@@ -24,6 +46,13 @@
         End Set
     End Property
 
+    ''' <summary>
+    ''' This is property is used to override the property Checked.
+    ''' If this property is read, there is nothing different.
+    ''' But if this property is assigned, the property Broken is checked first.
+    ''' If the property Broken is True, the property Checked does not work.
+    ''' </summary>
+    ''' <returns></returns>
     Public Shadows Property Checked As Boolean
         Get
             Return MyBase.Checked
@@ -33,6 +62,9 @@
         End Set
     End Property
 
+    ''' <summary>
+    ''' This is the constructor.
+    ''' </summary>
     Public Sub New()
         Broken = False
     End Sub
--- Toolkit/FormMain.vb
+++ Toolkit/FormMain.vb
@@ -1,6 +1,16 @@
-﻿Public Class FormMain
+﻿''' <summary>
+''' This form is invisible.
+''' This form cannot run without parameter. If there is no parameter, the form will be disposed.
+''' There is only one parameter of this form. In other words, this form is used to do only one thing:
+''' move the hosts file to "X:\Windows\System32\drivers\etc\hosts".
+''' </summary>
+Public Class FormMain
+    ''' <summary>
+    ''' This is the constructor.
+    ''' </summary>
     Public Sub New()
         With Me
+            ' The following three lines are used to hide the main form.
             .Size = New Size(0, 0)
             .Opacity = 0
             .ShowInTaskbar = False
@@ -17,14 +27,21 @@
         End
     End Sub
 
+    ''' <summary>
+    ''' This sub is used to move the '.\hosts' file to "X:\Windows\System32\drivers\etc\hosts".
+    ''' </summary>
     Private Sub MoveHostsFile()
+        ' The path of the source file.
         Dim HostsFilePath As String = Environment.SystemDirectory & "\drivers\etc\hosts"
+        ' The path of the distination file.
         Dim TempHostsFilePath As String = ".\hosts"
 
+        ' If the source file does not exist, exit this sub.
         If Not My.Computer.FileSystem.FileExists(TempHostsFilePath) Then
             Exit Sub
         End If
 
+        ' Move the source file to the distination file.
         My.Computer.FileSystem.MoveFile(TempHostsFilePath, HostsFilePath, True)
     End Sub
 End Class
