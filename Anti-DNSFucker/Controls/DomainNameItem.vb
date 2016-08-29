10d8b78ad45163b79f50ff43606ec54ece287085
zqmillet <zqmillet@qq.com>
2016-08-29 11:17:08 +0800

    Add Some Comments

--- Anti-DNSFucker/Controls/DomainNameItem.vb
+++ Anti-DNSFucker/Controls/DomainNameItem.vb
@@ -29,6 +29,84 @@ Public Class DomainNameItem
     ''' <param name="sender"></param>
     ''' <param name="e"></param>
     Public Event SelectCheckedChanged(sender As Object, e As EventArgs)
+    ''' <summary>
+    ''' This property is used to indicate if this item is selected.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Selected As Boolean
+        Get
+            Return CheckBoxSelect.Checked
+        End Get
+        Set(value As Boolean)
+            CheckBoxSelect.Checked = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' This property is used to indicate if this item is enabled.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Shadows Property Enabled As Boolean
+        Get
+            Return CheckBoxEnable.Checked
+        End Get
+        Set(value As Boolean)
+            CheckBoxEnable.Checked = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' This property is used to indicate if get IPv4 address of the domain name of this item.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property GetIPv4Address As Boolean
+        Get
+            Return CheckBoxGetIPv4Address.Checked
+        End Get
+        Set(value As Boolean)
+            CheckBoxGetIPv4Address.Checked = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' This property is used to indicate if get IPv6 address of the domain name of this item.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property GetIPv6Address As Boolean
+        Get
+            Return CheckBoxGetIPv6Address.Checked
+        End Get
+        Set(value As Boolean)
+            CheckBoxGetIPv6Address.Checked = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' This property is the domain name of this item.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property DomainName As String
+        Get
+            Return TextBoxDomainName.Text
+        End Get
+        Set(value As String)
+            TextBoxDomainName.Text = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' This writeonly property is used to enable/disable the CheckedChanged event of CheckBoxSelect.
+    ''' </summary>
+    Public WriteOnly Property CheckBoxSelectEventEnabled As Boolean
+        Set(value As Boolean)
+            If value Then
+                AddHandler CheckBoxSelect.CheckedChanged, AddressOf CheckBox_CheckedChanged
+            Else
+                RemoveHandler CheckBoxSelect.CheckedChanged, AddressOf CheckBox_CheckedChanged
+            End If
+        End Set
+    End Property
+
 
     ' This CheckBox is used to indicate if this item is selected.
     Private CheckBoxSelect As CheckBox
@@ -53,91 +131,14 @@ Public Class DomainNameItem
     ' This color is backcolor of the TextBox if the IP address is not resolved successfully.
     Private FailedColor As Color = Color.FromArgb(&HFFFF7F7F)
     ' This delegate refer to sub SetCheckBoxGetIPvXEnable.
+    ' This array list is used to store the column styles of this item.
+    Private ColumnNameList As ArrayList
     Delegate Sub DelegateSetCheckBoxGetIPvXEnable()
 
     ''' <summary>
-    ''' This function is used to resolve the IP addresses of the domain name of this item.
+    ''' This is the constructor.
     ''' </summary>
-    ''' <returns>If the domain name is resolved successfully, return true, else, return false.</returns>
-    Public Function Resolve() As Boolean
-        Dim IP As New IP
-        IP.ResolveByNEU(DomainName)
-        IPv4Address = IP.IPv4Address
-        IPv6Address = IP.IPv6Address
-
-        Me.Invoke(New DelegateSetCheckBoxGetIPvXEnable(AddressOf SetCheckBoxGetIPvXEnable))
-        IsResolved = True
-
-        Return Not (IPv4Address.Trim = "" And IPv6Address.Trim = "")
-    End Function
-
-    Private Sub SetCheckBoxGetIPvXEnable()
-        CheckBoxGetIPv4Address.Broken = IPv4Address.Trim = ""
-        CheckBoxGetIPv6Address.Broken = IPv6Address.Trim = ""
-
-        CheckBoxGetIPv4Address.Enabled = CheckBoxEnable.Checked
-        CheckBoxGetIPv6Address.Enabled = CheckBoxEnable.Checked
-
-        TextBoxDomainName.BackColor = If(CheckBoxGetIPv4Address.Broken And CheckBoxGetIPv6Address.Broken, FailedColor, SuccessfulColor)
-    End Sub
-
-    Public Property Selected As Boolean
-        Get
-            Return CheckBoxSelect.Checked
-        End Get
-        Set(value As Boolean)
-            CheckBoxSelect.Checked = value
-        End Set
-    End Property
-
-    Public Shadows Property Enabled As Boolean
-        Get
-            Return CheckBoxEnable.Checked
-        End Get
-        Set(value As Boolean)
-            CheckBoxEnable.Checked = value
-        End Set
-    End Property
-
-    Public Property GetIPv4Address As Boolean
-        Get
-            Return CheckBoxGetIPv4Address.Checked
-        End Get
-        Set(value As Boolean)
-            CheckBoxGetIPv4Address.Checked = value
-        End Set
-    End Property
-
-    Public Property GetIPv6Address As Boolean
-        Get
-            Return CheckBoxGetIPv6Address.Checked
-        End Get
-        Set(value As Boolean)
-            CheckBoxGetIPv6Address.Checked = value
-        End Set
-    End Property
-
-    Public Property DomainName As String
-        Get
-            Return TextBoxDomainName.Text
-        End Get
-        Set(value As String)
-            TextBoxDomainName.Text = value
-        End Set
-    End Property
-
-    Public ColumnNameList As ArrayList
-
-    Public WriteOnly Property CheckBoxSelectEventEnabled As Boolean
-        Set(value As Boolean)
-            If value Then
-                AddHandler CheckBoxSelect.CheckedChanged, AddressOf CheckBox_CheckedChanged
-            Else
-                RemoveHandler CheckBoxSelect.CheckedChanged, AddressOf CheckBox_CheckedChanged
-            End If
-        End Set
-    End Property
-
+    ''' <param name="ColumnStyleList"></param>
     Public Sub New(ByVal ColumnStyleList As ArrayList)
         With Me
             .Height = ItemHeight
@@ -207,23 +208,72 @@ Public Class DomainNameItem
         End With
     End Sub
 
+    ''' <summary>
+    ''' This function is used to resolve the IP addresses of the domain name of this item.
+    ''' </summary>
+    ''' <returns>If the domain name is resolved successfully, return true, else, return false.</returns>
+    Public Function Resolve() As Boolean
+        Dim IP As New IP
+        IP.ResolveByNEU(DomainName)
+        IPv4Address = IP.IPv4Address
+        IPv6Address = IP.IPv6Address
+
+        Me.Invoke(New DelegateSetCheckBoxGetIPvXEnable(AddressOf SetCheckBoxGetIPvXEnable))
+        IsResolved = True
+
+        Return Not (IPv4Address.Trim = "" And IPv6Address.Trim = "")
+    End Function
+
+    ''' <summary>
+    ''' This sub is used to set the properties Broken, Enable, and BackColor of this item according to the result of resolving.
+    ''' </summary>
+    Private Sub SetCheckBoxGetIPvXEnable()
+        CheckBoxGetIPv4Address.Broken = IPv4Address.Trim = ""
+        CheckBoxGetIPv6Address.Broken = IPv6Address.Trim = ""
+
+        CheckBoxGetIPv4Address.Enabled = CheckBoxEnable.Checked
+        CheckBoxGetIPv6Address.Enabled = CheckBoxEnable.Checked
+
+        TextBoxDomainName.BackColor = If(CheckBoxGetIPv4Address.Broken And CheckBoxGetIPv6Address.Broken, FailedColor, SuccessfulColor)
+    End Sub
+
+    ''' <summary>
+    ''' This sub will be triggered when the text of TextBoxDomainName is changed.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub TextBoxDomainName_TextChanged(sender As Object, e As EventArgs)
         IsResolved = False
     End Sub
 
+    ''' <summary>
+    ''' This sub will be triggered when check state of CheckBoxEnable or CheckBoxSelect is changed.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub CheckBox_CheckedChanged(sender As Object, e As EventArgs)
         If sender Is CheckBoxEnable Then
-            TextBoxDomainName.Enabled = CheckBoxEnable.Checked
+            ' If this sub is triggered by CheckBoxEnable.
 
+            ' Set the Broken of CheckBoxGetIPv4Address and CheckBoxGetIPv6Address.
             CheckBoxGetIPv4Address.Broken = IPv4Address.Trim = ""
             CheckBoxGetIPv6Address.Broken = IPv6Address.Trim = ""
+            ' Then, set the Enabled of CheckBoxGetIPv4Address, CheckBoxGetIPv6Address, and TextBoxDomainName.
             CheckBoxGetIPv4Address.Enabled = CheckBoxEnable.Checked
             CheckBoxGetIPv6Address.Enabled = CheckBoxEnable.Checked
+            TextBoxDomainName.Enabled = CheckBoxEnable.Checked
         ElseIf sender Is CheckBoxSelect Then
+            ' If this sub is triggered by CheckBoxSelect.
+            ' Raise the event SelectCheckedChanged.
             RaiseEvent SelectCheckedChanged(sender, e)
         End If
     End Sub
 
+    ''' <summary>
+    ''' This sub is used to draw vertical line on this item.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub Me_Paint(sender As Object, e As PaintEventArgs)
         Dim Graphics As Graphics = Me.CreateGraphics
         Dim Pen As New Pen(Color.LightGray, 1)
@@ -237,6 +287,12 @@ Public Class DomainNameItem
         End With
     End Sub
 
+    ''' <summary>
+    ''' This function is used to get the value of control by name of the control.
+    ''' If there is no this control, return an empty string.
+    ''' </summary>
+    ''' <param name="Name"></param>
+    ''' <returns></returns>
     Public Function GetValueByName(ByVal Name As String) As String
         For Each Control As Control In Me.Controls
             If Control.Name = Name Then
